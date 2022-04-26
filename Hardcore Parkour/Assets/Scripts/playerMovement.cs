d50c50906100e57977f617dbba0b8c898a426b0a
Vishal Nangia <35602127+Zerokool786@users.noreply.github.com>
2022-04-26 20:33:20 +0100

    implemented jumping
    the player jump needs improving it's to do with the player height

--- Hardcore Parkour/Assets/Scripts/playerMovement.cs
+++ Hardcore Parkour/Assets/Scripts/playerMovement.cs
@@ -6,16 +6,26 @@ public class playerMovement : MonoBehaviour
 {
     [Header("Movement")]
 
-    public Transform orientation;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    
+    bool isJumping;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    public float moveSpeed;
+    public float groundDrag;
+
 
     [Header("Ground Check")] //check if player is grounded
     public float playerHeight;
     public LayerMask whatIsGround; //a layermask for whatIsGround tag 
-    bool grounded;                 //conditional
+    
+    bool isGrounded;               //conditional
 
-    public float groundDrag;
-
-    public float moveSpeed;
+    public Transform orientation;
 
     float horizontalInput;
     float verticalInput;
@@ -23,24 +33,29 @@ public class playerMovement : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        //isJumping = true;
+
+        resetJump();
     }
 
     private void Update()
     {
-
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.2f + 0.5f, whatIsGround);  //perform ground check 
+        
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);  //perform ground check 
         keyInput();
-
+        speedController();
         //apply the drag
-        if (grounded)
+        if (isGrounded)
             rb.drag = groundDrag;
         // not grounded then put the value to zero
         else                
-            rb.drag = 0;  
+            rb.drag = 0;
+        
     }
 
     private void FixedUpdate()
@@ -52,14 +67,61 @@ public class playerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        
+        //check if space is pressed and is jumping and player is grounded
+        if (Input.GetKey(jumpKey) && isJumping && isGrounded)  
+        {
+            isJumping = false;
+            
+            Jump();
+
+            // reset jump function with a delay and do a continous jump when holding down space key
+            Invoke(nameof(resetJump), jumpCooldown);
+        }
     }
 
     private void movePlayer()
     {
         //walking in the direcion relative to where player is looking
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+
+        if (isGrounded) // on the ground
+
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); //add force to the rigidbody 
+
+        else if (!isGrounded)  //if player is in air multiply movement speed with air multiplier 
+
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void speedController()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit the velocity if required
+        if (flatVelocity.magnitude > moveSpeed)   //if player goes faster than movement speed                    
+        {
+            //estimate the max velocity
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+
+            //apply max velocity to the rigidbody
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+
+
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //apply force upwards only once
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
         
-        //add force to the rigidbody 
-        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+        
+    }
+
+    private void resetJump()
+    {
+        isJumping = true;
     }
 }
