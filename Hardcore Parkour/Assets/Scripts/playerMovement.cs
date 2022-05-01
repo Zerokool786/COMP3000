286cd1d29d526775ba3242bd2ed60a4003ceb66c
Vishal Nangia <35602127+Zerokool786@users.noreply.github.com>
2022-05-01 22:11:23 +0100

    playerMovement update
    sprinting and crouching can be achieved by pressing left shift and left control respectively

--- Hardcore Parkour/Assets/Scripts/playerMovement.cs
+++ Hardcore Parkour/Assets/Scripts/playerMovement.cs
@@ -12,16 +12,38 @@ public class playerMovement : MonoBehaviour
     
     bool isJumping;
 
+    public float sprintSpeed;
+    public float walkingSpeed;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    public float defaultYScale;
+
+
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
-    public float moveSpeed;
+    private float moveSpeed;
     public float groundDrag;
 
 
     [Header("Ground Check")] //check if player is grounded
     public float playerHeight;
     public LayerMask whatIsGround; //a layermask for whatIsGround tag 
+
+
+    public movementState currentState;    //stores current state of the player
+    public enum movementState
+    {
+        crouching,
+        walking,
+        sprinting,
+        air
+
+    }
     
     bool isGrounded;               //conditional
 
@@ -33,12 +55,15 @@ public class playerMovement : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
+    
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-        //isJumping = true;
+        isJumping = true;
+        defaultYScale = transform.localScale.y;  //saves default y scale of the rigidbody
 
         resetJump();
     }
@@ -46,9 +71,10 @@ public class playerMovement : MonoBehaviour
     private void Update()
     {
         
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);  //perform ground check 
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.9f + 0.5f, whatIsGround);  //perform ground check 
         keyInput();
         speedController();
+        statesHandler();
         //apply the drag
         if (isGrounded)
             rb.drag = groundDrag;
@@ -63,6 +89,31 @@ public class playerMovement : MonoBehaviour
         movePlayer();
     }
 
+    private void statesHandler()
+    {
+        if (Input.GetKey(crouchKey))   ///crouching if left control pressed 
+        {
+            currentState = movementState.crouching;   //change current state to crouching
+            moveSpeed = crouchSpeed;      
+        }
+
+        if(isGrounded && Input.GetKey(sprintKey))  //Sprinting if left shift is pressed
+        {
+            currentState = movementState.sprinting;  //change current state to sprinting
+            moveSpeed = sprintSpeed; 
+        }
+
+        else if (isGrounded)    //walking if grounded
+        {
+            currentState = movementState.walking; //change current state to walking
+            moveSpeed = walkingSpeed;         
+        }
+
+        else      
+        {
+            currentState = movementState.air;    //player is air if above statements are not executed 
+        }
+    }
     private void keyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -77,6 +128,20 @@ public class playerMovement : MonoBehaviour
 
             // reset jump function with a delay and do a continous jump when holding down space key
             Invoke(nameof(resetJump), jumpCooldown);
+        }
+
+        //start crouching if left control is pressed 
+        if (Input.GetKeyDown(crouchKey))
+        {
+            //shrink rb downwards to start crouching
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        //stop crouching by checking if left control key is released
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, defaultYScale, transform.localScale.z);
         }
     }
 
@@ -113,6 +178,7 @@ public class playerMovement : MonoBehaviour
     }
     private void Jump()
     {
+        // reset y velocity to zero so to jump the exact same height 
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         //apply force upwards only once
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
