4f5872588ddb7cb908eecf10cbf038b7ddf3a181
Vishal Nangia <35602127+Zerokool786@users.noreply.github.com>
2022-05-17 12:20:08 +0100

    out of bounds fixed

---
+++ Hardcore Parkour/Assets/Scripts/Teleporting.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        thePlayer.transform.position = teleportTarget.position;
+    }
+
+
+}
