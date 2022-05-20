f96781cc6d5563c9ce382af08e9e15c74f9ded69
Vishal Nangia <35602127+Zerokool786@users.noreply.github.com>
2022-05-20 01:06:26 +0100

    Implemented Wall running
    Player can wall run for a small amount of time

--- Hardcore Parkour/Assets/Scripts/WallRun.cs
+++ Hardcore Parkour/Assets/Scripts/WallRun.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class WallRun : MonoBehaviour
 {
     [Header("References")]
+    public playerMovement movePlayer;
     public Transform orientation;
     public Rigidbody rb;
-    public LayerMask whatsIsWall;
+    public LayerMask whatIsWall;
 
 
     [Header("Climbing")]
@@ -17,7 +18,7 @@ public class WallRun : MonoBehaviour
 
     private bool climbing;
 
-    [Header("Detection")]
+    [Header("Wall Detection")]
     public float detectionLength;
     public float sphereCastRadius;
     public float maxWallLookAngle;
@@ -27,10 +28,69 @@ public class WallRun : MonoBehaviour
 
     private bool wallFront;
 
-    private void WallCheck()
+    private void Update()
     {
+        StateMachine();
+        WallCheck();
 
+        if (climbing) ClimbMovement();
     }
 
+    private void WallCheck()
+    {
+        wallFront = Physics.SphereCast(transform.position, sphereCastRadius, orientation.forward, out frontWallHit, detectionLength, whatIsWall);
+        wallLookAngle = Vector3.Angle(orientation.forward, -frontWallHit.normal);
+        
+        if (movePlayer.isGrounded)
+        {
+            //reset climb timer if player is grounded
+            climbTimer = maxClimbTime; 
+        }
+    }
+
+    private void StateMachine()
+    {
+
+            //Wall Jump
+            //if the key is pressed and the look angle is less than the maximum look angle
+            if (wallFront && Input.GetKey(KeyCode.W) && wallLookAngle < maxWallLookAngle)
+        {
+
+            //if not climbing and have climb timer remaining call begin climbing
+            if (!climbing && climbTimer > 0) BeginClimbing(); 
+
+            //if countdown is above zero start climbing 
+            if (climbTimer > 0) climbTimer -= Time.deltaTime;
+
+            //if countdown is below zero stop climbing
+            if (climbTimer < 0) StopClimbing();
+
+        }
+
+            else
+        
+        {
+            if (climbing) StopClimbing(); //if player is not climbing stop all active climbs
+        
+        }
+    
+    
+    }
+
+    private void BeginClimbing()
+    {
+        climbing = true;
+    }
+
+
+    private void ClimbMovement()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, climbSpeed, rb.velocity.z); //set Y velocity of the player to climb speed
+    }
+
+    private void StopClimbing()
+    {
+        climbing = false;
+    }
 
 }
--- Hardcore Parkour/Assets/Scripts/playerMovement.cs
+++ Hardcore Parkour/Assets/Scripts/playerMovement.cs
@@ -49,7 +49,7 @@ public class playerMovement : MonoBehaviour
 
     }
     
-    bool isGrounded;               //conditional
+    public bool isGrounded;              
 
     public Transform orientation;
 
