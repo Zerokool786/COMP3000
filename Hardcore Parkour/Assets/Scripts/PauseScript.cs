27ef21e69a0775c54aa9e5a9d14776d8f0c35973
Vishal Nangia <35602127+Zerokool786@users.noreply.github.com>
2022-05-22 20:56:37 +0100

    game can be paused

---
+++ Hardcore Parkour/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1) //if not paused
+                Time.timeScale = 0; // pause the game
+
+            else
+                Time.timeScale = 1; //else resume the game
+        }
+	}
+}
